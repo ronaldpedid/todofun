613fe2c74d4e95fdc2038da99e640a69e85c4a2a
ronaldpedid <ronaldpedid@live.com>
2019-09-16 18:23:20 -0500

    Updates

---
+++ precache-manifest.48690087f874b557836abc887c0dc22f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc6dac8b86bd7a9e9fc3499da794c9b1",
+    "url": "/tadone/index.html"
+  },
+  {
+    "revision": "8bdec23c078234b8d352",
+    "url": "/tadone/static/css/main.55370b91.chunk.css"
+  },
+  {
+    "revision": "93b0074a0e6fd3b21065",
+    "url": "/tadone/static/js/2.243972ba.chunk.js"
+  },
+  {
+    "revision": "8bdec23c078234b8d352",
+    "url": "/tadone/static/js/main.9639e7b8.chunk.js"
+  },
+  {
+    "revision": "d758b3dd570da951530c",
+    "url": "/tadone/static/js/runtime~main.62056beb.js"
+  }
+]);
